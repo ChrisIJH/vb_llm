9c5f7bfb237ef1a038d1233079cc83ae16cfb5c0
Chris Hwang <ih138@columbia.edu>
2024-12-29 13:52:48 -0500

    good code

--- vb_chatgpt_app1.py
+++ vb_chatgpt_app1.py
@@ -1,63 +1,46 @@
-"""
-https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps
-
-streamlit run streamlit_chat_app_1.py  --server.port 8053
-"""
 import streamlit as st
-import openai
 from openai import OpenAI
 
+st.title("Eunseo ChatGPT App")
 
-st.title("Chat with me!")
-st.write(openai.__version__)
+# with st.chat_message("user"):
+#     st.write("Hello 👋")
+
+# with st.chat_message("assistant"):
+#     st.write("Hello human")
 
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
-
-
-
 if "openai_model" not in st.session_state:
-    st.session_state['openai_model'] = "gpt-3.5-turbo"
-
-
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-
+# Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# with st.chat_message("user"):
-#     st.write("Hello!")
-# with st.chat_message("assistant"):
-#     st.write("Hello!")
-#     st.bar_chart({"A": 1, "B": 2, "C": 3})
 
-
-# prompt = st.chat_input("Say something...", key="input_1")
-
-if prompt:= st.chat_input("Say something..."):
-    # st.write(f"User: {prompt}")
-    # st.markdown(f"User: {prompt}")
+prompt = st.chat_input("Say something")
+if prompt:
     with st.chat_message("user"):
         st.markdown(prompt)
-    st.session_state.messages.append({"role": "user", "content": prompt})
-
-    # response block
-    response = f"Echo: {prompt}"
-
+    #     # st.write(f"User has sent the following prompt: {prompt}")
+    st.session_state.messages.append({"role": "user", "content":prompt})
+    
     with st.chat_message("assistant"):
         stream = client.chat.completions.create(
-            model=st.session_state['openai_model'],
+            model=st.session_state["openai_model"],
             messages=[
-                {"role": m['role'], "content": m['content']} 
+                {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
             ],
-            stream = True
+            stream=True,
         )
         response = st.write_stream(stream)
     st.session_state.messages.append({"role": "assistant", "content": response})
-
+    
+    
     st.write(st.session_state)
